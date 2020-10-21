43c5437c31a1a5883ecdd5b05f767ea6c54d3f75
RaymiIrahola <raymiirahola@gmail.com>
2020-10-21 15:19:29 -0300

    Comienzo del proyecto

---
+++ vs/DominioBolillero/DominioBolillero/Bolillero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DominioBolillero
+{
+    public class Bolillero
+    {
+        public int jugarNVeces { get; set; }
+        public List<bolillas>;
+
+        public Bolillero (int unJugarNVeces)
+        {
+            jugarNVeces = unJugarNVeces;
+        }
+    }
+}
