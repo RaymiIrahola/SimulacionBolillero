d5eda291671d8c0881991d854fa9106e74b84f88
RaymiIrahola <raymiirahola@gmail.com>
2020-10-28 15:22:15 -0300

    Creacion de Simulacion y Las acciones del bolillero

--- vs/DominioBolillero/DominioBolillero/Bolillero.cs
+++ vs/DominioBolillero/DominioBolillero/Bolillero.cs
@@ -6,11 +6,43 @@ namespace DominioBolillero
     public class Bolillero
     {
         public int jugarNVeces { get; set; }
-        public List<bolillas>;
+        
+        public List<byte> bolillaAdentro { get; set; }
+        public List<byte> bolillaAfuera { get; set; }
+        Random r = new Random(DateTime.Now.Millisecond);
 
-        public Bolillero (int unJugarNVeces)
+        public Bolillero()
         {
-            jugarNVeces = unJugarNVeces;
+
         }
+
+        public Bolillero(byte inicio, byte fin)
+        {
+            List<byte> bolillaAdentro = new List<byte>();
+            List<byte> bolillaAfuera = new List<byte>();
+            int cargarBolillero = new int();
+        }
+        
+
+        public byte SacarBolilla(byte unaBolillaAfuera)
+        {
+            
+        }
+
+        public void Reingresar()
+        {
+            
+        }
+
+        private void CargartBolillero(byte inicio, byte fin)
+        {
+
+            for (byte i = inicio; i < fin; i++)
+            {
+
+            }
+        }
+        
+
     }
 }
---
+++ vs/DominioBolillero/DominioBolillero/Simulacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DominioBolillero
+{
+    public class Simulacion
+    {
+        Bolillero bolillero = new Bolillero();
+
+        public Simulacion(byte inicio, byte fin)
+        {
+
+        }
+
+        public bool Jugar()
+        {
+
+        }
+
+        public long JugarN()
+        {
+
+        }
+
+    }
+}
